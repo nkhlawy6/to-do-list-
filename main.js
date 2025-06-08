aca76ab9cded510d08d27475d78a5001165b3c90
emauser1 <emauser1@mci.ext>
2025-06-08 23:04:26 +0300

    save task from input value

---
+++ main.js
@@ -0,0 +1,20 @@
+let checkBtn = document.getElementById("checkBtn");
+let tasks = document.getElementById("tasks");
+let input1=document.getElementById('input1');
+let form=document.querySelector('form');
+form.addEventListener("submit", (eo) => {
+  eo.preventDefault();
+  let task = `      
+  <div class="task">
+        <span class="icon-star icon"></span>
+        <p>${input1.value}</p>
+        <div>
+          <span class="icon-trash-o icon"></span>
+          <!-- <span class="icon-heart"></span> -->
+          <span class="icon-angry icon"></span>
+        </div>
+    </div>`;
+  tasks.innerHTML+=task;
+  input1.value='';
+  console.log(eo);
+});
